4fd14caefaeaa32e32e4862b4f3c70803c708dd2
Ermachkova Julia <miscanth@gmail.com>
2024-09-20 18:19:21 +0300

    Almost all is done

--- main.py
+++ main.py
@@ -1,45 +1,46 @@
 import ffmpeg
 from pathlib import Path
 from constants import MAIN_FOLDER, RESULT_FOLDER
-import os
+import os, sys
+import re
 
 
 BASE_DIR = Path(__file__).resolve().parent
-folder_dir = BASE_DIR / MAIN_FOLDER
+data_folder = BASE_DIR / MAIN_FOLDER
 
+# Создаём отдельный каталог для видео файлов.
 os.makedirs(RESULT_FOLDER, exist_ok=True)
 
-for path, folders, files in os.walk(folder_dir, topdown=True):
+
+video_name_list = []
+
+for path, folders, files in os.walk(data_folder, topdown=True):
+    for file in files:
+        num_part = ''
+        file, separator = file.split('.jpg')
+        file = file[::-1]
+        for i in file:
+            if re.search(r'\d', i):
+                num_part = num_part + i
+            elif re.search(r'\D', i):
+                num_part = num_part + i
+                break
+        separator, name = file.split(num_part)
+        name = name[::-1]
+        if name not in video_name_list:
+            video_name_list.append(name)
+
+print(video_name_list)
+
+for path, folders, files in os.walk(data_folder, topdown=True):
     for folder in folders:
-        (
-            ffmpeg
-            .input(f'{path}/{folder}/*.jpg', pattern_type='glob', framerate=24)
-            .output(f'{RESULT_FOLDER}/{folder}.mp4')
-            .run()
-        )
-
-
-
-"""for folder in os.listdir(folder_dir):
-    (
-        ffmpeg
-        .input(f'{folder_dir}/{folder}/*.jpg', pattern_type='glob', framerate=24)
-        .output(f'{RESULT_FOLDER}/{folder}.mp4')
-        .run()
-    )
-"""
-
-"""for (root,dirs,files) in os.walk(folder_dir,topdown=True):
-  print("Directory path: %s"%root)
-  print("Directory Names: %s"%dirs)
-  print("Files Names: %s"%files)"""
-
-
-"""results_dir = BASE_DIR / RESULT_FOLDER
-results_dir.mkdir(exist_ok=True)"""
-
-
-"""if (images.endswith(".png")):
-    print(images)"""
-"""for image in os.listdir(folder):
-    if folder in image:"""
+        for sequence in video_name_list:
+                    try:
+                        (
+                            ffmpeg
+                            .input(f'{path}/{folder}/{sequence}*.jpg', pattern_type='glob', framerate=24)
+                            .output(f'{RESULT_FOLDER}/{sequence}.mp4')
+                            .run()
+                        )
+                    except:
+                         print('Oooops!')
