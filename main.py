f747daaac44ce60851d9d63de08159ecc3e1aac6
Ermachkova Julia <miscanth@gmail.com>
2024-09-20 20:15:57 +0300

    Fix sequence error check

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import ffmpeg
+import os
 from pathlib import Path
-from constants import MAIN_FOLDER, RESULT_FOLDER
-import os, sys
 import re
+from typing import List
+
+import ffmpeg
+
+from constants import MAIN_FOLDER, RESULT_FOLDER
 
 
 BASE_DIR = Path(__file__).resolve().parent
@@ -12,35 +15,46 @@ data_folder = BASE_DIR / MAIN_FOLDER
 os.makedirs(RESULT_FOLDER, exist_ok=True)
 
 
-video_name_list = []
+def find_sequences(path, folder) -> List[str]:
+    """
+    Поиск среди всех изображений в указанной папке
+    уникальных последовательностей.
+    """
+    video_name_list = []
 
-for path, folders, files in os.walk(data_folder, topdown=True):
-    for file in files:
-        num_part = ''
-        file, separator = file.split('.jpg')
-        file = file[::-1]
-        for i in file:
-            if re.search(r'\d', i):
-                num_part = num_part + i
-            elif re.search(r'\D', i):
-                num_part = num_part + i
-                break
-        separator, name = file.split(num_part)
-        name = name[::-1]
-        if name not in video_name_list:
-            video_name_list.append(name)
+    for file in os.listdir(f'{path}/{folder}'):
+        if (file.endswith(".jpg")):
+            num_part = ''
+            file, separator = file.split('.jpg')
+            file = file[::-1]
+            for i in file:
+                if re.search(r'\d', i):
+                    num_part += i
+                elif re.search(r'\D', i):
+                    num_part += i
+                    break
+            separator, name = file.split(num_part)
+            name = name[::-1]
+            if name not in video_name_list:
+                video_name_list.append(name)
+    return video_name_list
 
-print(video_name_list)
 
-for path, folders, files in os.walk(data_folder, topdown=True):
-    for folder in folders:
-        for sequence in video_name_list:
-                    try:
-                        (
-                            ffmpeg
-                            .input(f'{path}/{folder}/{sequence}*.jpg', pattern_type='glob', framerate=24)
-                            .output(f'{RESULT_FOLDER}/{sequence}.mp4')
-                            .run()
-                        )
-                    except:
-                         print('Oooops!')
+def main():
+    """Основная функция монтажа видео."""
+    for path, folders, files in os.walk(data_folder, topdown=True):
+        for folder in folders:
+            for sequence in find_sequences(path, folder):
+                try:
+                    (
+                        ffmpeg
+                        .input(f'{path}/{folder}/{sequence}*.jpg', pattern_type='glob', framerate=24)
+                        .output(f'{RESULT_FOLDER}/{sequence}.mp4')
+                        .run()
+                    )
+                except:
+                    print('Oooops!')
+
+
+if __name__ == "__main__":
+    main()
