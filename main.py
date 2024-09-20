4c22a3b79bd39b2a196474e79a4c3d8cb6e30651
Ermachkova Julia <miscanth@gmail.com>
2024-09-20 14:24:54 +0300

    Change README and main.py - add recursion

--- main.py
+++ main.py
@@ -9,15 +9,30 @@ folder_dir = BASE_DIR / MAIN_FOLDER
 
 os.makedirs(RESULT_FOLDER, exist_ok=True)
 
+for path, folders, files in os.walk(folder_dir, topdown=True):
+    for folder in folders:
+        (
+            ffmpeg
+            .input(f'{path}/{folder}/*.jpg', pattern_type='glob', framerate=24)
+            .output(f'{RESULT_FOLDER}/{folder}.mp4')
+            .run()
+        )
 
-for folder in os.listdir(folder_dir):
+
+
+"""for folder in os.listdir(folder_dir):
     (
         ffmpeg
         .input(f'{folder_dir}/{folder}/*.jpg', pattern_type='glob', framerate=24)
         .output(f'{RESULT_FOLDER}/{folder}.mp4')
         .run()
     )
+"""
 
+"""for (root,dirs,files) in os.walk(folder_dir,topdown=True):
+  print("Directory path: %s"%root)
+  print("Directory Names: %s"%dirs)
+  print("Files Names: %s"%files)"""
 
 
 """results_dir = BASE_DIR / RESULT_FOLDER
